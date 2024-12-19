a101f3b3c84718055ed68fd65fe0405fa1d42b99
Code2With-Pratik <pratikdhandare1204@gmail.com>
2024-12-20 01:44:37 +0530

    all files are set up to deploy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
@@ -24,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -32,7 +33,13 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 app.listen(PORT,()=>{
     connectDB();
