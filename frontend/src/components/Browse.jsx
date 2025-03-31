5905c0a09d38d2c3585b8aa1c612f9e72b4d684b
Code2With-Pratik <work.pratik1204@gmail.com>
2025-03-31 18:36:01 +0530

    Enhance Browse component layout for improved responsiveness and readability

--- frontend/src/components/Browse.jsx
+++ frontend/src/components/Browse.jsx
@@ -1,40 +1,41 @@
-import React, { useEffect } from 'react'
-import Navbar from './shared/Navbar'
+import React, { useEffect } from 'react';
+import Navbar from './shared/Navbar';
 import Job from './Job';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSearchedQuery } from '@/redux/jobSlice';
 import useGetAllJobs from '@/hooks/useGetAllJobs';
-import Footer from './shared/Footer'
-// const randomJobs = [1, 2,45];
+import Footer from './shared/Footer';
 
 const Browse = () => {
     useGetAllJobs();
-    const {allJobs} = useSelector(store=>store.job);
+    const { allJobs } = useSelector(store => store.job);
     const dispatch = useDispatch();
-    useEffect(()=>{
-        return ()=>{
+
+    useEffect(() => {
+        return () => {
             dispatch(setSearchedQuery(""));
-        }
-    },[])
+        };
+    }, []);
+
     return (
         <div>
             <Navbar />
-            <div className='max-w-7xl mx-auto my-10'>
-                <h1 className='font-bold text-xl my-10'>Search Results ({allJobs.length})</h1>
-                <div className='grid grid-cols-3 gap-4'>
-                    {
-                        allJobs.map((job) => {
-                            return (
-                                <Job key={job._id} job={job}/>
-                            )
-                        })
-                    }
-                </div>
+            <div className='max-w-7xl mx-auto my-10 px-4'>
+                {/* Responsive Heading */}
+                <h1 className='font-bold text-xl my-6 text-center sm:text-left'>
+                    Search Results ({allJobs.length})
+                </h1>
 
+                {/* Responsive Grid for Job Cards */}
+                <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+                    {allJobs.map((job) => (
+                        <Job key={job._id} job={job} />
+                    ))}
+                </div>
             </div>
             <Footer />
         </div>
-    )
-}
+    );
+};
 
-export default Browse
+export default Browse;
